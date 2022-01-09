bdbaa0746c0aae678fe27959cc4dbd6715e79cb6
Kenny Tse <kennytse91@gmail.com>
2022-01-08 17:59:44 -0800

    added navbar

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,11 +1,18 @@
-import './App.css';
+import './App.scss';
 import Background from './components/Background';
+import NavBar from './components/NavBar';
 
 const App = () => {
   return (
     <div className="App">
       <Background />
-      
+      <NavBar />
+
+
+      <div className="IntroContainer">
+        <header className="Name">KENNY TSE</header>
+        <p1 className="IntroParagraph">Full stack developer with a passion for creating apps that not only enhance the user experience but the internal employee experience as well. I’m excited to utilize my interpersonal skills to understand the customer and employee pain points and use that as the driving factor in app development.</p1>
+      </div>
     </div>
   );
 }
---
+++ portfolio/src/components/NavBar.jsx
@@ -0,0 +1,23 @@
+import './navBar.scss';
+
+const NavBar = (props) => {
+
+  return (
+
+    <div id="NavBar">
+      <div>
+        Introduction
+      </div>
+      <div>
+        Projects
+      </div>
+      <div>
+        Placeholder
+      </div>
+    </div>
+
+  )
+
+}
+
+export default NavBar;
