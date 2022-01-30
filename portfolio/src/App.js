95d2207a89109d3465f2e4870abbc9b742c763be
Kenny Tse <kennytse91@gmail.com>
2022-01-30 15:11:50 -0800

    added mobile support for intro and project section

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -25,7 +25,7 @@ const App = () => {
         </div>
 
         <div className="VideoHolder">
-          <iframe className="VideoToShow" width="3000" height="315" src="https://www.youtube.com/embed/lKQDpHSCSDM" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+          <iframe className="VideoToShow" src="https://www.youtube.com/embed/lKQDpHSCSDM" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
           <a className="videoDescription">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit.Curabitur lacinia tincidunt scelerisque.Maecenas sit amet pellentesque neque.Donec eu porttitor diam.Morbi imperdiet velit ante, vitae dignissim justo pretium nec.Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.Nullam ut risus sit amet mi finibus porttitor.Aliquam tincidunt tempor sapien eu hendrerit.Donec euismod sagittis orci.Phasellus malesuada ipsum a erat consequat pretium.Nulla tristique diam eu faucibus efficitur.Integer ultricies aliquet nibh, ut ultricies ligula vestibulum et.Fusce non enim sit amet massa tincidunt gravida.Pellentesque mi elit, ultrices sed magna et, laoreet mollis magna.Donec congue a urna sed venenatis.Maecenas sed eros ex.</a>
         </div>
